fc3225cd915dbbea3dedec78b980b14f3bd39243
lamach1n3 <taillefer.jf@gmail.com>
2021-03-19 13:19:37 -0400

    kdjfls

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
-  post "/graphql", to: "graphql#execute"
-  get "/graphql", to: "graphql#execute"
+  post "/graphiql", to: "graphiql#execute"
+  get "/graphiql", to: "graphiql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
